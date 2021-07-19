388be09245d55d3c9862a75b7eaf3f15e7c46fdd
Jennifer Portiño <jennifer.portinof@utem.cl>
2021-07-18 21:15:45 -0400

    Delete: Archivos que no se usan

--- Otros/js/statusUser.js
+++
@@ -1,8 +0,0 @@
-firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-        document.getElementById("cuenta").style.display = "block";
-        document.getElementById("cerrarSesion").style.display = "block";
-    } else {
-        // No user is signed in.
-    }
-});

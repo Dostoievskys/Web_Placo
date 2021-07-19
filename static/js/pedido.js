24fe7efd30d8de0d783c299e5e882c03461e84d7
Jennifer Portiño <jennifer.portinof@utem.cl>
2021-07-19 03:48:54 -0400

    Se elimina imagen de pedido

--- static/js/pedido.js
+++ static/js/pedido.js
@@ -10,7 +10,7 @@ window.createProducts = function createProducts(){
     for(var i = 0; i <prod.length; i++){
         /*disenio en js de catalogo dinamico */
         total = total + (parseFloat(prod[i].cant)*parseFloat(prod[i].price));
-        contenido+= '<li class="list-group-item">'+prod[i].producto+'<p>'+prod[i].cant+' x '+prod[i].price+'</p> <img class="Pimagen" src="/img/cafe1.jpg" ></li>'
+        contenido+= '<li class="list-group-item">'+prod[i].producto+'<p>'+prod[i].cant+' x '+prod[i].price+'</p></li>' //<img class="Pimagen" src="/img/cafe1.jpg" >
     }
     contenido+= '<li class="list-group-item">Total: '+total+' USD</li>'
     contenido+='<input type="hidden" name="cmd" value="_xclick"><input type="hidden" name="amount" value="'+total+'"><input type="hidden" name="currency_code" value="USD"><input type="hidden" name="business" value="diegotapialeal@hotmail.com"></input>'
